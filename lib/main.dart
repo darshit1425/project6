fc0e22ea365d8625eb8d8f330abf9574dc2913de
HP <darshitmendapara1425@gmail.com>
2023-01-27 10:27:04 +0530

    Stack App

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project6/code.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: code(),
+    ),
+  );
+}
